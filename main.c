74f4f141d49ff4a444bbd355708287424e04907a
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-11-30 09:37:52 +0200

    For testing the shell

---
+++ main.c
@@ -0,0 +1,5 @@
+//
+// Created by cr3dzu on 11/30/23.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by cr3dzu on 11/30/23.
+//
+
+#ifndef SIMPLE_SHELL_MAIN_H
+#define SIMPLE_SHELL_MAIN_H
+
+#endif //SIMPLE_SHELL_MAIN_H
